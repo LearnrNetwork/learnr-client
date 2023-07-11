ea1b7a7a37257d11e7df914a40414dc62a42e99f
Manish Shivabhakti <manish@learnr.app>
2023-07-11 16:54:43 +0545

    Added the books page

--- src/components/books/Book.tsx
+++ src/components/books/Book.tsx
@@ -1,19 +1,25 @@
 import Image from 'next/image';
-import Badge from '../badge/Badge';
+import Badge from '@/components/badge/Badge';
 import Link from 'next/link';
 
 export default function Book({ title, author, cover, tags, href }: any) {
 	return (
-		<div className='flex lg:w-48 md:w-full'>
+		<div className='flex overflow-hidden bg-white border border-gray-200 rounded-lg lg:w-72 md:w-full'>
 			<div className='flex flex-col'>
 				{/* book cover */}
-				<div className='self-center w-44'>
-					<Image src={cover} alt={title} width={500} height={500} />
+				<div className='self-center w-full bg-green-300 border-b border-gray-200'>
+					<Image
+						src={cover}
+						alt={title}
+						className='px-20 py-8'
+						width={500}
+						height={500}
+					/>
 				</div>
 				{/* book title */}
-				<div className='flex flex-col'>
+				<div className='flex flex-col px-4 py-4'>
 					<Link href={href}>
-						<h2 className='mt-2 uppercase whitespace-pre-wrap cursor-pointer hover:text-primary'>
+						<h2 className='uppercase whitespace-pre-wrap cursor-pointer hover:text-primary'>
 							{title}
 						</h2>
 					</Link>
@@ -24,7 +30,11 @@ export default function Book({ title, author, cover, tags, href }: any) {
 					{/* book tags */}
 					<div className=''>
 						{tags.map((tag: { _id: string; name: string }) => (
-							<Badge key={tag._id} content={tag.name} className='ml-1' />
+							<Badge
+								key={tag._id}
+								content={tag.name}
+								className='mr-1 text-gray-500 uppercase'
+							/>
 						))}
 					</div>
 				</div>
--- src/pages/bookism/index.tsx
+++ src/pages/bookism/index.tsx
@@ -1,5 +1,6 @@
 import Book from '@/components/books/Book';
-import SidebarLayout from '@/components/layouts/SidebarLayout';
+import UserLayout from '@/components/layouts/UserLayout';
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 
 // test
 const books = [
@@ -33,7 +34,7 @@ const books = [
 		author: 'David Thomas, Andrew Hunt',
 		href: '/bookism/the-pragmatic-programmer',
 		cover:
-			'https://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&MarketPlace=US&ASIN=B01MYMUGQL&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL800_&tag=allencheng-20',
+			'https://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&MarketPlace=US&ASIN=B079WM7KLS&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL800_&tag=allencheng-20',
 		tags: [
 			{ _id: '1', name: 'Programming' },
 			{ _id: '2', name: 'Software Engineering' },
@@ -103,20 +104,64 @@ const books = [
 
 export default function BookismPage() {
 	return (
-		<SidebarLayout>
-			<div className='flex flex-wrap md:justify-around lg:justify-start lg:w-2/3 md:w-full'>
-				{books.map((book: any) => (
-					<div key={book._id} className='flex mb-4 ml-4'>
-						<Book
-							title={book.title}
-							author={book.author}
-							cover={book.cover}
-							tags={book.tags}
-							href={book.href}
-						/>
+		<UserLayout>
+			{/* a search field with option */}
+			<div className='flex items-center justify-center w-full h-72'>
+				<div className='relative w-1/2'>
+					<label htmlFor='Search'>
+						<p className='sr-only'>Search for the book you want</p>
+					</label>
+					<div className='absolute flex items-center justify-center h-full w-14'>
+						<MagnifyingGlassIcon className='w-6 h-6 font-semibold text-gray-500' />
 					</div>
-				))}
+					<input
+						type='text'
+						placeholder='Search the book you want'
+						className='w-full py-4 pl-12 rounded-full'
+					/>
+				</div>
 			</div>
-		</SidebarLayout>
+			{/* a list of books */}
+			<div className='lg:w-3/4 md:w-full'>
+				{/* Based on your interest */}
+				<div className=''>
+					<h2 className='mb-4 text-2xl tracking-wide'>
+						Based on your interest
+					</h2>
+					<div className='flex overflow-x-scroll md:justify-around lg:justify-start'>
+						{books.map((book: any) => (
+							<div key={book._id} className='flex mb-4 ml-4'>
+								<Book
+									title={book.title}
+									author={book.author}
+									cover={book.cover}
+									tags={book.tags}
+									href={book.href}
+								/>
+							</div>
+						))}
+					</div>
+				</div>
+				{/* What people are reading */}
+				<div className='my-8'>
+					<h2 className='mb-4 text-2xl tracking-wide'>
+						What people are reading
+					</h2>
+					<div className='flex overflow-x-scroll md:justify-around lg:justify-start'>
+						{books.map((book: any) => (
+							<div key={book._id} className='flex mb-4 ml-4'>
+								<Book
+									title={book.title}
+									author={book.author}
+									cover={book.cover}
+									tags={book.tags}
+									href={book.href}
+								/>
+							</div>
+						))}
+					</div>
+				</div>
+			</div>
+		</UserLayout>
 	);
 }
