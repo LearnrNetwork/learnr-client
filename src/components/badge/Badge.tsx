21a67b6cb2f65f91cd5d5ee7c732e84be6f1adfa
Manish Shivabhakti <manish@learnr.app>
2023-08-03 19:33:42 +0545

    Made className and onClick optional

--- src/components/badge/Badge.tsx
+++ src/components/badge/Badge.tsx
@@ -3,15 +3,19 @@ import classNames from '@/utils/classNames';
 export default function Badge({
 	content,
 	className,
+	onClick,
 }: {
 	content: string;
-	className: string;
+	className?: string;
+	onClick?: (...args: any) => void;
 }) {
 	return (
 		<div
+			onClick={onClick}
 			className={classNames(
-				className +
-					' inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800'
+				className,
+				onClick ? 'cursor-pointer' : '',
+				' inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800'
 			)}
 		>
 			{content}
