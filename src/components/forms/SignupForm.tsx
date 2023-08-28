d789e3285bd3890ad5f7ecb006e704d00175a3a3
Manish Shivabhakti <manish@learnr.app>
2023-08-28 08:36:40 +0545

    Fixed typo

--- src/components/forms/SignupForm.tsx
+++ src/components/forms/SignupForm.tsx
@@ -136,7 +136,7 @@ export default function SignupForm() {
 									type='submit'
 									className='flex justify-center w-full px-4 py-2 text-sm font-medium text-white border border-transparent rounded-md shadow-sm bg-primary hover:bg-primary focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2'
 								>
-									Sign in
+									Sign up
 								</button>
 							</div>
 						</form>
